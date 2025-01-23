356ac51ae7a1d1b7c3bfe0a35f03773410c90ee4
cyancaesar <29235530+cyancaesar@users.noreply.github.com>
2025-01-23 20:25:02 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'auth_user',
+      password: 'auth_pass',
+      database: 'auth_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoffeesModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
