69efe68863d48b456b362b3b9670a6703b76552b
cyancaesar <29235530+cyancaesar@users.noreply.github.com>
2025-01-23 21:29:29 +0300

    bare minimum implementations of sign in/sign up

---
+++ src/iam/authentication/authentication.service.ts
@@ -0,0 +1,51 @@
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.userRepository.save(user);
+    } catch (error) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (error.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException();
+      }
+      throw error;
+    }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const existingUser = await this.userRepository.findOneBy({
+      email: signInDto.email,
+    });
+    if (!existingUser) throw new UnauthorizedException();
+
+    const isValidPassword = await this.hashingService.compare(
+      signInDto.password,
+      existingUser.password,
+    );
+    if (!isValidPassword) throw new UnauthorizedException();
+
+    // TODO: JWT and sheeit.
+    return true;
+  }
+}
